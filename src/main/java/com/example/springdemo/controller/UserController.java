7e718e68827a250182876d673372e8282722a184
gulangjie <15251757297@163.com>
2019-11-12 23:53:50 +0800

    修改userController

--- src/main/java/com/example/springdemo/controller/UserController.java
+++ src/main/java/com/example/springdemo/controller/UserController.java
@@ -1,13 +1,34 @@
 package com.example.springdemo.controller;
 
+import com.example.springdemo.pojo.JsonResult;
+import com.example.springdemo.pojo.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 
-@RestController
+
+@RestController                   //RestController = @Controller + @ResponseBody
+@RequestMapping(value="/user")
 public class UserController {
-    @RequestMapping(value="/hello")
-    public String hello(){
-        return "Hello Spring Boot";
+
+    @RequestMapping(value="/getUser")
+    public User getUser(){
+        User user = new User();
+        user.setName("imooc");
+        user.setAge(18);
+        user.setBirthday(new Date());
+        user.setPassword("imooc");
+        return user;
+    }
+
+    @RequestMapping(value="/getUserJson")
+    public JsonResult getUserJson(){
+        User user = new User();
+        user.setName("imooc");
+        user.setAge(18);
+        user.setBirthday(new Date());
+        user.setPassword("imooc");
+        return JsonResult.ok(user);
     }
 }
